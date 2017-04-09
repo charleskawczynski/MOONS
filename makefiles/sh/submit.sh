d46faeb32fdd7aa1f54094fef0cb2bbd49f60813
Charlie Kawczynski <CharlieKawczynski@gmail.com>
2017-04-08 19:39:08 -0700

    Bug fixes: 	Fixed submit.sh (gcc.4.9.3 --> gcc/4.9.3)
    Changes:
    	Some small changes to hoffman2 jump start commands
    
    Next, pushing to Hoffman

--- makefiles/sh/submit.sh
+++ makefiles/sh/submit.sh
@@ -11,24 +11,20 @@
 # Notify at beginning and end of job
 # #$ -m n # for never
 #$ -m bea
-
 echo ""
 echo "MOONS.exe started on: "` hostname -s `
 echo "MOONS.exe started at: "` date `
 echo ""
-
 #
 # Run the user program
 #
-
 . /u/local/Modules/default/init/modules.sh
 module load intel/13.cs > /dev/null 2>&1
-module load gcc.4.9.3 > /dev/null 2>&1
+module load gcc/4.9.3 > /dev/null 2>&1
 #
 echo ""
 /usr/bin/time ./MOONS.exe > ./MOONS.exe.output.$JOB_ID 2>&1
 #
 echo ""
 echo "MOONS.exe finished at: "` date `
-
 #### sumit.sh END ##### Then (just for the first time you created the file) issue:
